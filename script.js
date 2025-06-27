84cda6cf988423768f7683a4e18c9e6ac2020aa3
maazaamir <149050052+maazaamir@users.noreply.github.com>
2025-06-27 16:49:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelector(".menu-icon").addEventListener("click", menutoggle);
 });
+var  productImg =document.getElementById("productImg");
+var  smallImg =document.getElementById("small-img")
+
+smallImg[0].onclick = function()
+{
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function()
+{
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function()
+{
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function()
+{
+    productImg.src = smallImg[3].src;
+}
+var productImg = document.getElementById("productImg");
+var smallImgs = document.getElementsByClassName("small-img");
+
+for (let i = 0; i < smallImgs.length; i++) {
+    smallImgs[i].onclick = function() {
+        productImg.src = smallImgs[i].src;
+    }
+}
