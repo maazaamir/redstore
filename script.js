88c7ffe726382e06f2a4cb1072384b2c2c41dad8
maazaamir <149050052+maazaamir@users.noreply.github.com>
2026-02-25 06:14:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 >>>>>>> 225f221 (Initial clean commit after Git reset)
@@ -60,3 +61,67 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 >>>>>>> a3efaf5 (Initial commit)
 >>>>>>> 225f221 (Initial clean commit after Git reset)
+=======
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 225f221 (Initial clean commit after Git reset)
+document.addEventListener("DOMContentLoaded", function () {
+    var MenuItems = document.getElementById("MenuItems");
+
+    function menutoggle() {
+        if (MenuItems.style.maxHeight === "0px" || MenuItems.style.maxHeight === "") {
+            MenuItems.style.maxHeight = "200px"; // Expand menu
+        } else {
+            MenuItems.style.maxHeight = "0px"; // Collapse menu
+        }
+    }
+
+    document.querySelector(".menu-icon").addEventListener("click", menutoggle);
+});
+var  productImg =document.getElementById("productImg");
+var  smallImg =document.getElementById("small-img")
+
+smallImg[0].onclick = function()
+{
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function()
+{
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function()
+{
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function()
+{
+    productImg.src = smallImg[3].src;
+}
+var productImg = document.getElementById("productImg");
+var smallImgs = document.getElementsByClassName("small-img");
+
+for (let i = 0; i < smallImgs.length; i++) {
+    smallImgs[i].onclick = function() {
+        productImg.src = smallImgs[i].src;
+    }
+}
+<<<<<<< HEAD
+=======
+=======
+document.addEventListener("DOMContentLoaded", function () {
+    var MenuItems = document.getElementById("MenuItems");
+
+    function menutoggle() {
+        if (MenuItems.style.maxHeight === "0px" || MenuItems.style.maxHeight === "") {
+            MenuItems.style.maxHeight = "200px"; // Expand menu
+        } else {
+            MenuItems.style.maxHeight = "0px"; // Collapse menu
+        }
+    }
+
+    document.querySelector(".menu-icon").addEventListener("click", menutoggle);
+});
+>>>>>>> a3efaf5 (Initial commit)
+>>>>>>> 225f221 (Initial clean commit after Git reset)
+>>>>>>> 77beef24b42bbe6a94b2b20c6455b3c0d5031887
